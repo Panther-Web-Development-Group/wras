8014579b2b50431aeed293978b299df62e3c394d
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2023-10-18 13:34:02 -0400

    Adding the WRAS app

--- docs/index.js
+++ docs/index.js
@@ -1,11 +1,18 @@
 const header = document.querySelector("#header");
-const navigation = header.querySelector("#header-inner");
+const navigation = header.querySelector("#header-top");
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        console.log(entry.intersectionRatio);
-        navigation.classList.toggle("flyover", !entry.isIntersecting);
+        navigation.classList.toggle("overlay", !entry.isIntersecting);
     });
-}, { threshold: 0.8 });
+}, { threshold: 0.4 });
 
 observer.observe(header);
+
+window.addEventListener("click", event => {
+    /** @type {?HTMLInputElement} */
+    const checkbox = document.querySelector("input#trigger-input");
+    if (!event.target.closest("#header-top") && checkbox.checked) {
+        checkbox.checked = !checkbox.checked;
+    }
+});
